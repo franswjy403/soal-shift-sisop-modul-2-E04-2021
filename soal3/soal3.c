143933e9adcc6df0f704f61b44466e1eaa9aff05
frans0416 <franswijaya403@gmail.com>
2021-04-23 10:14:18 +0700

    Last update soal3.c

--- soal3/soal3.c
+++ soal3/soal3.c
@@ -78,69 +78,70 @@ int main(int argc, char* argv[]) {
     close(STDERR_FILENO);
     int status, status2, status3, status4;
     while (1) {
-        pid_t cid1, cid2, cid3, cid4, cid5, cid6;
+        pid_t cid1, cid2, cid3, cid4, cid5, cid6, cidGlob;
         time_t times = time(NULL);
         struct tm* date1 = localtime(&times);
         char now[35];
         strftime(now, 30, "%Y-%m-%d_%H:%M:%S", date1);
 
-        cid1 = fork();
-        if (cid1 < 0) exit(EXIT_FAILURE);
-        if (cid1 == 0) {
-            char *ag[] = {"/bin/mkdir", "-p", now, NULL};
-            execv("/bin/mkdir", ag);
-        }
-
-        int status;
-        
-        while (wait(&status)>0);
-        cid2 = fork();
-        if (cid2 < 0) exit(EXIT_FAILURE);
-        if (cid2 == 0){
-            chdir(now);
-            int i;
-            for (i=0;i<10;i++){
-                time_t times2 = time(NULL);
-                struct tm* date2 = localtime(&times2);
-                char now2[35];
-                strftime(now2, 30, "%Y-%m-%d_%H:%M:%S", date2);
-                char photo[1000];
-                sprintf(photo, "https://picsum.photos/%ld", (times2 % 1000) + 50);
-
-                cid3 = fork();
-                if (cid3 < 0) exit(0);
-                if (cid3 == 0){
-                    char *ag[] = {"wget", photo, "-O", now2, "-o", "/dev/null", NULL};
-                    execv("/usr/bin/wget", ag);
-                }
-                sleep(5);
+        cidGlob = fork();
+        if (cidGlob < 0)exit(EXIT_FAILURE);
+        if (cidGlob == 0){
+            cid1 = fork();
+            if (cid1 < 0) exit(EXIT_FAILURE);
+            if (cid1 == 0) {
+                char *ag[] = {"/bin/mkdir", "-p", now, NULL};
+                execv("/bin/mkdir", ag);
             }
 
-            if (i>=10){
-                char temp[100];
-                strcpy(temp, "Download Success");
-                strcpy(temp, caesarChiper(temp));
-                FILE *fp = fopen("status.txt", "w+");
-                fprintf(fp, "%s", temp);
-                fclose(fp);
-            }
+            int status;
             
-            while(wait(&status3)>0);
-            chdir("..");
-            char zipName[100];
-            sprintf(zipName, "%s.zip", now);
+            while (wait(&status)>0);
+            cid2 = fork();
+            if (cid2 < 0) exit(EXIT_FAILURE);
+            if (cid2 == 0){
+                chdir(now);
+                int i;
+                for (i=0;i<10;i++){
+                    time_t times2 = time(NULL);
+                    struct tm* date2 = localtime(&times2);
+                    char now2[35];
+                    strftime(now2, 30, "%Y-%m-%d_%H:%M:%S", date2);
+                    char photo[1000];
+                    sprintf(photo, "https://picsum.photos/%ld", (times2 % 1000) + 50);
 
-            cid5 = fork();
-            if (cid5 < 0) exit(EXIT_FAILURE);
-            if (cid5 == 0){
+                    cid3 = fork();
+                    if (cid3 < 0) exit(0);
+                    if (cid3 == 0){
+                        char *ag[] = {"wget", photo, "-O", now2, "-o", "/dev/null", NULL};
+                        execv("/usr/bin/wget", ag);
+                    }
+                    sleep(5);
+                }
+
+                if (i>=10){
+                    char temp[100];
+                    strcpy(temp, "Download Success");
+                    strcpy(temp, caesarChiper(temp));
+                    FILE *fp = fopen("status.txt", "w+");
+                    fprintf(fp, "%s", temp);
+                    fclose(fp);
+                }
+                
+                while(wait(&status3)>0);
+                chdir("..");
+                char zipName[100];
+                sprintf(zipName, "%s.zip", now);
+
                 char *ag[] = {"zip", "-r", zipName, now, NULL};
                 execv ("/bin/zip", ag);
+                
             }
-
             while(wait(&status4)>0);
-            char *ag[] = {"rm", "-r", now, NULL};
-            execv("/bin/rm", ag);
+                char *ag[] = {"rm", "-r", now, NULL};
+                execv("/bin/rm", ag);
         }
+        
         sleep(40);
     }
 }
